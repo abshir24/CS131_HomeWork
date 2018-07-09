f057ecd3ebc308f46a8c3f11c1b56aa619083f36
Abshir <abshirm@live.com>
2018-07-09 16:52:08 -0700

    Quiz2

---
+++ AbshirMohamedQuiz2.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  131WorkSpace
+//
+//  Created by Abshir Mohamed on 6/26/18.
+//  Copyright © 2018 Abshir Mohamed. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(){
+    float average;
+    int student1=85,student2=86,student3=92,sum=student1+student2+student3;
+    average =sum/3.0;
+    cout << "Student 1: " << student1<<endl;
+    cout << "Student 2: " << student2<<endl;
+    cout << "Student 3: " << student3<<endl;
+    cout << "Average: " << average<<endl;
+    return 0;
+}
+
+
+
