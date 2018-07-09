6bfefb9b468192cbd9398b9e0b02fa145cf47426
Abshir <abshirm@live.com>
2018-07-09 16:51:20 -0700

    intial commit

---
+++ AbshirMohamedHelloWorld.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  131WorkSpace
+//
+//  Created by Abshir Mohamed on 6/26/18.
+//  Copyright © 2018 Abshir Mohamed. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    
+    cout << "My name is Abshir\nToday is 06/26/2018\nI'm writing a program in C++ now!\n";
+ 
+    return 0;
+}
+
+
+
