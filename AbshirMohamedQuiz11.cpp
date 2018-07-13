f37bcff3a682e08f5dff8275d2bc731cfd12ad93
Abshir <abshirm@live.com>
2018-07-12 17:38:15 -0700

    quiz 11

---
+++ AbshirMohamedQuiz11.cpp
@@ -0,0 +1,30 @@
+/*
+ Abshir Mohamed
+ 7-11-2018
+ HW3
+ */
+
+
+#include <iostream>
+#include<string>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    int sum = 0,s = 0;
+    
+    while(s<=100)
+    {
+        sum+=s;
+        s+=2;
+    }
+    
+    cout<<"Sum is: "<<sum<<endl;
+    
+    return 0;
+}
+
+
+
+
