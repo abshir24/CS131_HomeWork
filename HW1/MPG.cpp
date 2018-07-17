cb98f4e493a838a36eb238183d5e2209d0e8e3d2
Abshir <abshirm@live.com>
2018-07-16 19:14:37 -0700

    hw1 part 2 added

---
+++ HW1/MPG.cpp
@@ -0,0 +1,21 @@
+/*
+ Abshir Mohamed
+ 6-27-2018
+ HW1
+ */
+
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    const int gallons = 15;
+    float MPG = 375.0 / gallons;
+    
+    cout<<"This car gets  " << MPG << " miles per gallon "<<endl;
+    
+    return 0;
+}
+
+
+
