88f31e5a02c155014b0014d073f6e9e9456ed258
Abshir <abshirm@live.com>
2018-07-16 19:14:14 -0700

    hw1 folder added with first assignment

---
+++ HW1/salesPrediction.cpp
@@ -0,0 +1,21 @@
+/*
+ Abshir Mohamed
+ 6-27-2018
+ HW1
+ */
+
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    const float generate = .58;
+    float eastCoastAmount = 8600000 * generate;
+    
+    cout<<"The East Coast Division will generate  " << eastCoastAmount << " dollars in sales this year "<<endl;
+    
+    return 0;
+}
+
+
+
