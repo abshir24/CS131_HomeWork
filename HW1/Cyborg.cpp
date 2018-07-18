77d7b248532ae8df82341113e40de676fa6d5732
Abshir <abshirm@live.com>
2018-07-17 18:56:30 -0700

    hw1 pt 5

---
+++ HW1/Cyborg.cpp
@@ -0,0 +1,34 @@
+/*
+ Abshir Mohamed
+ 6-27-2018
+ HW1
+ */
+
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int integer = 1;
+    
+    char character = 'l';
+    
+    float floater = 2.0;
+    
+    double doubler = 9.5;
+    
+    cout<<"An integer is " << sizeof(integer)<<" bytes of memory"<<endl;
+    
+    cout<<"A character is " << sizeof(character)<<" bytes of memory"<<endl;
+    
+    cout<<"A float is " << sizeof(floater)<<" bytes of memory"<<endl;
+    
+    cout<<"A double is " << sizeof(doubler)<<" bytes of memory"<<endl;
+
+    return 0;
+}
+
+
+
+
