206b94a681042af63f27559193095bb308af9fa7
Abshir <abshirm@live.com>
2018-07-17 18:56:06 -0700

    hw1 pt 4

---
+++ HW1/AnnualPay.cpp
@@ -0,0 +1,24 @@
+/*
+ Abshir Mohamed
+ 6-27-2018
+ HW1
+ */
+
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    float payAmount = 2200.0;
+    int payPeriods = 26;
+    float annualPay = payAmount*payPeriods;
+    
+    cout<<"The employee gets payed a total of " << annualPay <<" a year"<<endl;
+    
+    return 0;
+}
+
+
+
+
