859421bce0061053e0a68e89b3f3e379dc2ed7f7
Abshir <abshirm@live.com>
2018-07-19 11:56:58 -0700

    hw1 part7

---
+++ HW1/EnergyDrinkConsumption.cpp
@@ -0,0 +1,25 @@
+/*
+ Abshir Mohamed
+ 6-27-2018
+ HW1
+ */
+
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    float energyDrinkCustomers = 16500 *.15;
+    float citrusFlavoredCustomers = energyDrinkCustomers *.58;
+    
+    cout<<"Customers that purchase one or more energy drinks = " << energyDrinkCustomers<<endl;
+    
+    cout<<"Customers that prefer citrus drinks = " << citrusFlavoredCustomers <<endl;
+    
+    return 0;
+}
+
+
+
+
