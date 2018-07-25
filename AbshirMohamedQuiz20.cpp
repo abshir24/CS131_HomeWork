e55dc2e0a52a8955acd94f62a9974a01c9a7834d
Abshir <abshirm@live.com>
2018-07-25 16:25:16 -0700

    Quiz 20

---
+++ AbshirMohamedQuiz20.cpp
@@ -0,0 +1,68 @@
+/*
+ Abshir Mohamed
+ 7-24-2018
+ HW4
+ */
+
+
+#include <iostream>
+#include<string>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+double getSales(string);
+
+void findHighest(double,double,double,double);
+
+double Northeast = getSales("Northeast");
+double Southeast = getSales("Southeast");
+double Northwest = getSales("Northwest");
+double Southwest = getSales("Southwest");
+
+int main()
+{
+    findHighest(Northeast,Southeast,Northwest,Southwest);
+    
+    return 0;
+    
+}
+
+double getSales(string division)
+{
+    double sales;
+    
+    do{
+        cout<<"What are the quarterly sales for the " << division << " division? (sales cannot be a negative number)"<<endl;
+        
+        cin>>sales;
+    }while(sales<0);
+    
+    
+    
+    return sales;
+}
+
+void findHighest(double NE,double SE , double NW, double SW)
+{
+    string division;
+    
+    double max1 = max(NE,SE),
+    max2 = max(NW,SW),
+    winner = max(max1,max2);
+    
+    if(winner == Northeast) division = "Northeast";
+    
+    else if(winner == Southeast) division = "Southeast";
+    
+    else if(winner == Northwest) division = "Northwest";
+    
+    else division = "Southwest";
+    
+    cout<<"The winner is the  "<< division << " division with a final total of $"<< winner<<endl;
+}
+
+
+
+
+
