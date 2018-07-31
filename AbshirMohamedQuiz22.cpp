503bd7cdc4c8ee37426f366a10b0c569c5749b02
Abshir <abshirm@live.com>
2018-07-30 19:23:44 -0700

    quiz22

---
+++ AbshirMohamedQuiz22.cpp
@@ -0,0 +1,39 @@
+/*
+ Abshir Mohamed
+ 7-24-2018
+ HW4
+ */
+
+
+#include <iostream>
+#include<string>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    const int NUM_FISH = 8;
+    
+    int fish[NUM_FISH];
+    
+    
+    for(int i=0;i<NUM_FISH;i++)
+    {
+        cout<<"How many fish were caught by the fisherman "<< i+1 <<"?"<<endl;
+        cin>>fish[i];
+    }
+    
+    
+    
+    
+    
+    
+    return 0;
+    
+}
+
+
+
+
+
