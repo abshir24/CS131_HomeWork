20af97784a43c95e67f34455dfa12f810e1fe690
Abshir <abshirm@live.com>
2018-08-21 15:52:43 -0700

    handouts added. cs131 complete

---
+++ Handouts/Chapter 8 Quiz_27_ChargeAccountValidation.cpp
@@ -0,0 +1,121 @@
+// Chapter 8, Programming Challenge: Sorting Benchmarks
+#include <iostream>
+using namespace std;
+
+// Constant for array sizes
+const int SIZE = 20;
+
+// Function prototypes
+
+_______________________________
+ 
+_______________________________
+
+________________________________
+
+
+int main()
+{
+	int exchanges; // Number of exchanges made
+
+	// Two arrays with identical values
+	long accounts1[SIZE] =
+		{ 5658845,  4520125,  7895122,
+		  8777541,  8451277,  1302850,
+		  8080152,  4562555,  5552012,
+		  5050552,  7825877,  1250255,
+		  1005231,  6545231,  3852085,
+		  7576651,  7881200,  4581002 };
+        
+	long accounts2[SIZE] = 
+		{ 5658845,  4520125,  7895122,
+		  8777541,  8451277,  1302850,
+		  8080152,  4562555,  5552012,
+		  5050552,  7825877,  1250255,
+		  1005231,  6545231,  3852085,
+		  7576651,  7881200,  4581002 };
+        
+	// Sort accounts1 with bubble sort. The function
+	// returns the number of exchanges made.
+	// write your code here 
+
+	___________________
+   
+	// Display the number of exchanges made by the
+	// bubble sort.
+	cout << "\n" << exchanges 
+		 << " exchanges were made by Bubble Sort."
+		 << endl;
+
+	// Sort accounts2 with selection sort. The function
+	// returns the number of exchanges made.
+	// write your code here 
+	
+	_________________________
+
+	cout << "\n" << exchanges 
+		 <<  " exchanges were made by Selection Sort." 
+		 << endl;
+
+	//Call the showValue function twice to see your sorting result
+
+	/*
+
+	
+	display sorted arrays
+
+
+	*/	 
+
+	return 0;
+}
+
+// ********************************************************
+// The bubbleSort function performs the bubble sort on    *
+// the array and returns the number of exchanges made.    *
+// ********************************************************
+/*
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+
+// ********************************************************
+// The selectionSort function performs the selection sort *
+// on array and returns the number of exchanges made.     *
+// ********************************************************
+/*
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+
+
+
+
+//Define the showValue function
+/*
+
+Your showValue function
+
+
+
+*/
